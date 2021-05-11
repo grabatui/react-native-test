8c94a62fad7626d26720cc3af20dd3db2cde0fb5
Grabatui <anatoliy.bibanaev@gmail.com>
2021-05-11 23:20:40 +0500

    99. Start of the Navigation

--- rn-third-app/App.js
+++ rn-third-app/App.js
@@ -4,6 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import CategroriesScreen from './screens/CategoriesScreen';
+import CategoryMealsScreen from './screens/CategoryMealsScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import FiltersScreen from './screens/FiltersScreen';
+import MealScreen from './screens/MealScreen';
+
+
+const MealsNavigator = createStackNavigator();
+
+
 const fetchFonts = () => {
     return Font.loadAsync({
         'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -18,14 +31,20 @@ export default function App() {
         return <AppLoading
             startAsync={fetchFonts}
             onFinish={() => setFontLoaded(true)}
+            onError={(error) => console.error(error)}
         />
     }
 
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <MealsNavigator.Navigator initialRouteName="Categories">
+                <MealsNavigator.Screen name="Categories" component={CategroriesScreen} />
+                <MealsNavigator.Screen name="CategoryMeals" component={CategoryMealsScreen} />
+                <MealsNavigator.Screen name="Favorites" component={FavoritesScreen} />
+                <MealsNavigator.Screen name="Filters" component={FiltersScreen} />
+                <MealsNavigator.Screen name="Meal" component={MealScreen} />
+            </MealsNavigator.Navigator>
+        </NavigationContainer>
     );
 }
 
