a09c6afa66105015fe7180c31ec4b4daa0c0342c
Grabatui <anatoliy.bibanaev@gmail.com>
2021-05-22 20:33:25 +0500

    116. Tabs: Default and empty favorites

--- rn-third-app/App.js
+++ rn-third-app/App.js
@@ -7,6 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { enableScreens } from 'react-native-screens';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import CategroriesScreen from './screens/CategoriesScreen';
 import CategoryMealsScreen from './screens/CategoryMealsScreen';
@@ -20,6 +23,7 @@ import colors from './constants/colors';
 
 
 const MealsNavigator = createStackNavigator();
+const MealsTabNavigator = createBottomTabNavigator();
 
 enableScreens();
 
@@ -30,7 +34,7 @@ const fetchFonts = () => {
     });
 };
 
-export default function App() {
+const DefaultStackNavigator = () => {
     const [fontLoaded, setFontLoaded] = useState(false);
 
     if ( ! fontLoaded) {
@@ -42,47 +46,75 @@ export default function App() {
     }
 
     return (
+        <MealsNavigator.Navigator
+            initialRouteName="Categories"
+            screenOptions={{
+                headerStyle: styles.headerBar,
+                headerTintColor: Platform.OS == 'android' ? 'white' : colors.primaryColor,
+            }}
+        >
+            <MealsNavigator.Screen name="Categories" component={CategroriesScreen} />
+
+            <MealsNavigator.Screen
+                name="CategoryMeals"
+                component={CategoryMealsScreen}
+                options={({ route }) => ({
+                    title: route.params.category.title,
+                })}
+            />
+
+            <MealsNavigator.Screen
+                name="Meal"
+                component={MealScreen}
+                options={({ route }) => ({
+                    title: route.params.meal.title,
+                    headerRight: () => (
+                        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                            <Item
+                                title='Favorite'
+                                iconName='ios-star'
+                                onPress={() => {
+                                    console.log('Favorite!');
+                                }}
+                            />
+                        </HeaderButtons>
+                    ),
+                })}
+            />
+
+            <MealsNavigator.Screen name="Filters" component={FiltersScreen} />
+        </MealsNavigator.Navigator>
+    );
+};
+
+export default function App() {
+    return (
         <NavigationContainer>
-            <MealsNavigator.Navigator
-                initialRouteName="Categories"
-                screenOptions={{
-                    headerStyle: styles.headerBar,
-                    headerTintColor: Platform.OS == 'android' ? 'white' : colors.primaryColor,
+            <MealsTabNavigator.Navigator
+                tabBarOptions={{
+                    activeTintColor: colors.accentColor,
                 }}
             >
-                <MealsNavigator.Screen name="Categories" component={CategroriesScreen} />
-
-                <MealsNavigator.Screen
-                    name="CategoryMeals"
-                    component={CategoryMealsScreen}
-                    options={({ route }) => ({
-                        title: route.params.category.title,
-                    })}
-                />
-
-                <MealsNavigator.Screen
-                    name="Meal"
-                    component={MealScreen}
-                    options={({ route }) => ({
-                        title: route.params.meal.title,
-                        headerRight: () => (
-                            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                                <Item
-                                    title='Favorite'
-                                    iconName='ios-star'
-                                    onPress={() => {
-                                        console.log('Favorite!');
-                                    }}
-                                />
-                            </HeaderButtons>
+                <MealsTabNavigator.Screen
+                    name="Default"
+                    component={DefaultStackNavigator}
+                    options={{
+                        tabBarIcon: ({ color }) => (
+                            <Ionicons name='ios-restaurant' size={25} color={color} />
                         ),
-                    })}
+                    }}
                 />
 
-                <MealsNavigator.Screen name="Favorites" component={FavoritesScreen} />
-
-                <MealsNavigator.Screen name="Filters" component={FiltersScreen} />
-            </MealsNavigator.Navigator>
+                <MealsTabNavigator.Screen
+                    name="Favorites"
+                    component={FavoritesScreen}
+                    options={{
+                        tabBarIcon: ({ color }) => (
+                            <Ionicons name='ios-star' size={25} color={color} />
+                        ),
+                    }}
+                />
+            </MealsTabNavigator.Navigator>
         </NavigationContainer>
     );
 }
