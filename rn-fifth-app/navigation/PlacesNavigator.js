60b0060c0d7cde2994d2642137b75cceabdcd233
Grabatui <anatoliy.bibanaev@gmail.com>
2021-09-15 23:32:12 +0500

    203. New project - Addresses storage

---
+++ rn-fifth-app/navigation/PlacesNavigator.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import PlacesScreen from '../screens/PlacesScreen';
+import PlaceScreen from '../screens/PlaceScreen';
+import EditPlaceScreen from '../screens/EditPlaceScreen';
+import MapScreen from '../screens/MapScreen';
+
+import HeaderButton from '../components/HeaderButton';
+
+import colors from '../constants/colors';
+
+
+const Stack = createStackNavigator();
+
+const PlacesNavigator = () => (
+    <NavigationContainer>
+        <Stack.Navigator
+            initialRouteName="Places"
+            screenOptions={{
+                headerStyle: styles.wrapperHeader,
+                headerTintColor: Platform.OS === 'android' ? 'white' : colors.primary,
+            }}
+        >
+            <Stack.Screen
+                name="Places"
+                component={PlacesScreen}
+                options={({ navigation }) => ({
+                    headerTitle: 'Places',
+                    headerRight: () => (
+                        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                            <Item
+                                title="Add place"
+                                iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                                onPress={() => navigation.navigate('EditPlace')}
+                            />
+                        </HeaderButtons>
+                    )
+                })}
+            />
+            
+            <Stack.Screen
+                name="Place"
+                component={PlaceScreen}
+                options={() => ({
+                    headerTitle: 'Place',
+                })}
+            />
+            
+            <Stack.Screen
+                name="EditPlace"
+                component={EditPlaceScreen}
+                options={() => ({
+                    headerTitle: 'Edit place',
+                })}
+            />
+            
+            <Stack.Screen
+                name="Map"
+                component={MapScreen}
+                options={() => ({
+                    headerTitle: 'Map',
+                })}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
+
+const styles = StyleSheet.create({
+    wrapperHeader: {
+        backgroundColor: Platform.OS === 'android' ? colors.primary : '',
+    },
+});
+
+export default PlacesNavigator;
---
+++ rn-fifth-app/screens/PlaceScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+
+const PlaceScreen = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PlaceScreen;
