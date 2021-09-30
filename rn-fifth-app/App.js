86cdd929cab2024f27ab07877dc1a8473427d181
Grabatui <anatoliy.bibanaev@gmail.com>
2021-09-30 23:48:54 +0500

    211. SQLite connected [2]

--- rn-fifth-app/App.js
+++ rn-fifth-app/App.js
@@ -5,6 +5,17 @@ import thunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
 import placesReducer from './store/reducers/place';
+import { initDatabase } from './helpers/db'
+
+
+initDatabase()
+    .then(() => {
+        console.log('Database successfuly connected');
+    })
+    .catch((error) => {
+        console.log('Database exception');
+        console.error(error);
+    });
 
 
 const store = createStore(
