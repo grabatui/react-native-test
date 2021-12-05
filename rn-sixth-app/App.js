ed9055e7e97e892bef49848335d31116a16cf502
Grabatui <anatoliy.bibanaev@gmail.com>
2021-12-05 16:35:03 +0500

    259. New notifications app

---
+++ rn-sixth-app/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+export default function App() {
+    const triggerNotificationHandler = () => {
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: 'My first local notification',
+                body: 'This is the first local notification we are sending',
+            },
+            trigger: {
+                seconds: 3,
+            },
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Button
+                title="Trigger notification"
+                onPress={triggerNotificationHandler}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
