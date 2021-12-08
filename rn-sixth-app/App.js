f137669046a044cc4b9140e35e9ee876cd12a07f
Grabatui <anatoliy.bibanaev@gmail.com>
2021-12-08 21:29:49 +0500

    263. Notifications event listeners

--- rn-sixth-app/App.js
+++ rn-sixth-app/App.js
@@ -34,6 +34,24 @@ export default function App() {
         []
     );
 
+    useEffect(
+        () => {
+            const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(
+                (response) => console.log(response)
+            );
+
+            const foregroundSubscription = Notifications.addNotificationReceivedListener(
+                (notification) => console.log(notification)
+            );
+
+            return () => {
+                backgroundSubscription.remove();
+                foregroundSubscription.remove();
+            };
+        },
+        []
+    );
+
     const triggerNotificationHandler = () => {
         Notifications.scheduleNotificationAsync({
             content: {
